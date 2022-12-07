cb2506a756f8933e8393d76574f245f9cde93f5e
KULDEEP GAUTAM <118754536+kuldeeepp@users.noreply.github.com>
2022-12-07 12:33:34 +0530

    Add files via upload

---
+++ hour glass.py
@@ -0,0 +1,15 @@
+s=20
+for i in range(5,0,-1):
+    print(' '*s,end=' ')
+    for j in range(5,5-i,-1):
+        print("x",end=' ')
+    print()
+    s+=1
+s=24
+for i in range(1,6):
+    print(' '*s,end=' ')
+    for j in range(1,i+1):
+        print("x",end=' ')
+    print()
+    s-=1
+                                                                                                                    
